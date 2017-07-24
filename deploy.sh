2d9dc2560f3c7d53eb1be3bce59160391e9bc33d
Fernando Mayer <fernandomayer@gmail.com>
2017-07-24 17:40:57 -0300

    Include rsync file with directories to exclude from rsyncing

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ USER=fernandomayer
 HOST=leg.ufpr.br
 DIR=public_html/
 
-hugo && rsync -avz --delete docs/ ${USER}@${HOST}:~/${DIR}
+hugo && rsync -avz --delete --exclude-from 'rsync-exclude.txt' docs/ ${USER}@${HOST}:~/${DIR}
 
 exit 0
