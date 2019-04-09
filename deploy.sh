42d528ed77fa3e542204767bff7214bf59911ff7
Fernando Mayer <fernandomayer@gmail.com>
2019-04-09 20:55:48 -0300

    Include option to specify the server PORT

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 USER=fernandomayer
 HOST=leg.ufpr.br
 DIR=public_html/
+read -p 'PORTA: ' PORT
 
-Rscript -e 'source("build.R");build()' && hugo && rsync -avz --delete --exclude-from 'rsync-exclude.txt' docs/ ${USER}@${HOST}:~/${DIR}
+Rscript -e 'source("build.R");build()' && hugo && rsync -avz -e "ssh -p $PORT" --delete --exclude-from 'rsync-exclude.txt' docs/ ${USER}@${HOST}:~/${DIR}
 
 exit 0
